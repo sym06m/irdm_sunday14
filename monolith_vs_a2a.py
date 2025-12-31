eb6f47fd9fecd5526f95b9c5e43e9ea50705d44e
sym06m <symbat.otegen06@gmail.com>
2025-12-31 19:19:59 +0500

    Create monolith_vs_a2a.py

---
+++ monolith_vs_a2a.py
@@ -0,0 +1,14 @@
+def monolith():
+    return "One agent does everything"
+
+
+def a2a():
+    return [
+        "Planner agent",
+        "Validator agent",
+        "Executor agent"
+    ]
+
+
+print(monolith())
+print(a2a())
